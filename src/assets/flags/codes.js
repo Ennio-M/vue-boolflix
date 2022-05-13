974ba7189a23d43b1fdc745a7b28b77af6855d02
Ennio-M <marrese.ennio@gmail.com>
2022-05-13 12:32:51 +0200

    milestone 3

---
+++ src/assets/flags/codes.js
@@ -0,0 +1,15 @@
+const availableFlags = [
+    {
+        language: 'en',
+        countryCode: 'gb'
+    },
+    {
+        language: 'ja',
+        countryCode: 'jp'
+    },
+    {
+        language: 'ko',
+        countryCode: 'kr'
+    }
+]
+export default availableFlags;
--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,37 +1,57 @@
 <template>
     <li>
+        <div v-if="card.poster_path != null">
+            <img :src="imgUrl + imgWidth + card.poster_path" :alt="card.original_title + ' poster'">
+        </div>
+        <div v-else>
+            <h2>{{card.original_title ? card.original_title : card.original_name}}</h2>
+        </div>
+         <br>
         titolo originale: {{card.original_title ? card.original_title : card.original_name}} <br>
         titolo: {{card.title ? card.title : card.name}} <br>
-        lingua: <img :src="require('../flags/flagImgs/' + flagCode + '.png')" :alt="flagCode + ' flag'"> <br>
-        voto: {{card.vote_average}}
+        lingua: <img :src="require(`../assets/flags/flagImgs/${flagCode}.png`)" :alt="flagCode + ' flag'"> <br>
+        voto: <span v-if="card.vote_count > 0">
+            <i v-for="(n, i) in 5" :key="i" :class="n < transform(card.vote_average) ? 'fa-solid gold' : 'fa-regular'" class="fa-star"></i>
+        </span>
+        <span v-else>0 voti</span>
+        
     </li>
 </template>
 
 <script>
-//import flags from '../flags/flags.js'
+import codes from '../assets/flags/codes.js';
 
 export default {
     name: 'AppCard',
     data() {
         return {
-            
+            imgUrl: 'https://image.tmdb.org/t/p/',
+            imgWidth: 'w342',
+            flagCode: ''
         }
     },
     props: {
         card: Object
     },
-    computed: {
-        flagCode() {
-            if(this.card.original_language === 'en') {
-                return 'gb'
-            } else{
-                return this.card.original_language
+    methods: {
+        transform(vote) {
+            return parseInt(vote / 2)
+        }
+    },
+    created() {
+        for(let i = 0; i < codes.length; i++) {
+            if(codes[i].language === this.card.original_language) {
+                this.flagCode = codes[i].countryCode
+                return
             }
         }
+        this.flagCode = this.card.original_language
     }
 }
 </script>
 
 <style lang="scss" scoped>
-
+    .gold{
+        color: gold;
+    }
 </style>
